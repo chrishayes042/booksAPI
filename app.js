49286034adbefa1285be2d539fc51712f062ed24
chris <chrishayes042@gmail.com>
2022-08-01 18:01:11 -0600

    test

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
+function getBooks() {
+  let isbn = document.getElementById("insertBook").value;
 
-function getBooks(){
-    let isbn = document.getElementById('insertBook').value;
+  fetch(`https://openlibrary.org/isbn/${isbn}.json`)
+    .then((res) => res.json())
 
-    fetch(`https://openlibrary.org/isbn/${isbn}.json`)
-    .then(res => res.json())
-    
-    .then((res) => { 
-        let title = res;
-        title.map(`${full_title}`);
-            document.getElementById('titleData').value = title;          
-        })
-    .catch(err => console.warn(`error`, err));
-        
-    let 
+    .then((res) => {
+      let title = res;
+      title.map(`${full_title}`);
+      document.getElementById("titleData").value = title;
+    })
+    .catch((err) => console.warn(`error`, err));
 }
